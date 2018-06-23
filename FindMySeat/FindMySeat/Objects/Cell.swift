ce82160c8fa53eb3328ce220b83b578b85815256
aosanya <amosanya@gmail.com>
2018-06-23 13:38:27 +0200

    Add Label To Cells

--- FindMySeat/FindMySeat/Objects/Cell.swift
+++ FindMySeat/FindMySeat/Objects/Cell.swift
@@ -10,11 +10,21 @@ import SpriteKit
 
 
 class Cell : SKSpriteNode{
+    var label : SKLabelNode!
+    
     init(size : CGSize){
         super.init(texture: SKTexture(image: #imageLiteral(resourceName: "BlueCell")), color: UIColor.clear, size: size)
+        self.addLabel()
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+    }
+    
+    func addLabel(){
+        self.label = SKLabelNode(text: "10")
+        self.label.fontColor = UIColor.black
+        self.addChild(self.label!)
+        self.label.zPosition = self.zPosition  +  1
     }
 }
